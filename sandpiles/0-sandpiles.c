14115a44c1cfd4d329ad9b99da1cf7c9fa48e25c
ali-jin <alina.jin.contact@gmail.com>
2024-01-23 13:44:16 +0100

    fix index in all functions in the second loop

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -32,7 +32,7 @@ static void print_grid(int grid[3][3])
 void sum_sanpiles(int grid1[3][3], int grid2[3][3]) {
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; i++)
+        for (int j = 0; j < 3; j++)
         {
             grid1[i][j] += grid2[i][j];
         }
@@ -41,20 +41,19 @@ void sum_sanpiles(int grid1[3][3], int grid2[3][3]) {
 }
 
 /**
- * check_if_grid1_is_stable - check if grid1 is stable
+ * check_if_grid1_is_stable - check if grid1 is table
  * @grid1: first sandpile
  * @grid2: second sandpile
- * Return: returns 0 if stable. Otherwise 1.
+ * Return: returns 1 if stable. Otherwise 0.
 */
 int check_if_grid1_is_stable(int grid1[3][3], int grid2[3][3]) {
     int stable = 1;
 
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; i++)
+        for (int j = 0; j < 3; j++)
         {
             grid2[i][j] = grid1[i][j];
-
             if (grid1[i][j] > 3) {
                 stable = 0;
             }
@@ -70,20 +69,21 @@ int check_if_grid1_is_stable(int grid1[3][3], int grid2[3][3]) {
  * @grid2: second sandpile
 */
 void toppling_round(int grid1[3][3], int grid2[3][3]) {
+
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 3; i++)
+        for (int j = 0; j < 3; j++)
         {
             if (grid2[i][j] > 3) {
-                grid1[i][j] -= 4;
-                if (i - 1 >= 0)
+                if (i > 0)
                     grid1[i - 1][j] += 1;
-                if (i + 1 < 3)
+                if (i < 2)
                     grid1[i + 1][j] += 1;
-                if (j - 1 >= 0)
+                if (j > 0)
                     grid1[i][j - 1] += 1;
-                if (j  + 1 < 3)
+                if (j < 2)
                     grid1[i][j + 1] += 1;
+                grid1[i][j] -= 4;
             }
         }
     }
@@ -96,9 +96,7 @@ void toppling_round(int grid1[3][3], int grid2[3][3]) {
 */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
 
-
     sum_sanpiles(grid1, grid2);
-    
     while(!check_if_grid1_is_stable(grid1, grid2)) {
         print_grid(grid1);
         toppling_round(grid1, grid2);
--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-int main(void);
 void toppling_round(int grid1[3][3], int grid2[3][3]);
 int check_if_grid1_is_stable(int grid1[3][3], int grid2[3][3]);
 
