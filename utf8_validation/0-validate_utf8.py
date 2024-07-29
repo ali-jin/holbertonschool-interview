f499bcd1a7db35efced40cfd241039a68a64c0f8
ali-jin <alina.jin.contact@gmail.com>
2024-07-29 15:36:20 +0200

    change the function

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -16,21 +16,18 @@ def validUTF8(data):
     """
     num_of_bytes = 0
 
-    for value in data:
-        if num_of_bytes > 0:
-            if value >> 6 != 0b10:
+    for byte in data:
+        mask = 1 << 7
+        if num_of_bytes == 0:
+            while mask & byte:
+                num_of_bytes += 1
+                mask = mask >> 1
+            if num_of_bytes == 0:
+                continue
+            if num_of_bytes == 1 or num_of_bytes > 4:
                 return False
-            num_of_bytes -= 1
         else:
-            if value >> 7 == 0:
-                num_of_bytes = 0
-            elif value >> 5 == 0b110:
-                num_of_bytes = 1
-            elif value >> 4 == 0b1110:
-                num_of_bytes = 2
-            elif value >> 3 == 0b11110:
-                num_of_bytes = 3
-            else:
+            if not (byte & mask and not (byte & (mask >> 1))):
                 return False
-
+        num_of_bytes -= 1
     return num_of_bytes == 0
