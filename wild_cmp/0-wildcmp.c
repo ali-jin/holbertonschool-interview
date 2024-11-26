c98ec827ff90d8fa763b6a7f4cc6d8f3770aa432
ali-jin <alina.jin.contact@gmail.com>
2024-11-26 15:28:34 +0100

    0-wildcmp.c

---
+++ wild_cmp/0-wildcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+
+/**
+ * wildcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '\0' || wildcmp(s1, s2 + 1))
+			return (1);
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
+			return (1);
+	}
+	else if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+			return (1);
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
