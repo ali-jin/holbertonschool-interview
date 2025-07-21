e854be98178e40e921f854f2d9bd84eca9006825
ali-jin <alina.jin.contact@gmail.com>
2025-07-21 14:16:40 +0200

    betty code style fix

--- substring/substring.c
+++ substring/substring.c
@@ -11,43 +11,43 @@
  * Return: 1 if valid, 0 otherwise
  */
 int check_substring(char const *s, char const **words, int nb_words,
-                    int word_len, int *used, int start)
+					int word_len, int *used, int start)
 {
-    int i, j, found;
-    char *temp_word;
+	int i, j, found;
+	char *temp_word;
 
-    for (i = 0; i < nb_words; i++)
-        used[i] = 0;
+	for (i = 0; i < nb_words; i++)
+		used[i] = 0;
 
-    temp_word = malloc((word_len + 1) * sizeof(char));
-    if (!temp_word)
-        return (0);
+	temp_word = malloc((word_len + 1) * sizeof(char));
+	if (!temp_word)
+		return (0);
 
-    for (i = 0; i < nb_words; i++)
-    {
-        strncpy(temp_word, s + start + i * word_len, word_len);
-        temp_word[word_len] = '\0';
+	for (i = 0; i < nb_words; i++)
+	{
+		strncpy(temp_word, s + start + i * word_len, word_len);
+		temp_word[word_len] = '\0';
 
-        found = 0;
-        for (j = 0; j < nb_words; j++)
-        {
-            if (!used[j] && strcmp(temp_word, words[j]) == 0)
-            {
-                used[j] = 1;
-                found = 1;
-                break;
-            }
-        }
+		found = 0;
+		for (j = 0; j < nb_words; j++)
+		{
+			if (!used[j] && strcmp(temp_word, words[j]) == 0)
+			{
+				used[j] = 1;
+				found = 1;
+				break;
+			}
+		}
 
-        if (!found)
-        {
-            free(temp_word);
-            return (0);
-        }
-    }
+		if (!found)
+		{
+			free(temp_word);
+			return (0);
+		}
+	}
 
-    free(temp_word);
-    return (1);
+	free(temp_word);
+	return (1);
 }
 
 /**
@@ -62,57 +62,57 @@ int check_substring(char const *s, char const **words, int nb_words,
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
 {
-    int *indices = NULL;
-    int *used = NULL;
-    int word_len, s_len, i, count = 0;
-    int max_indices;
+	int *indices = NULL;
+	int *used = NULL;
+	int word_len, s_len, i, count = 0;
+	int max_indices;
 
-    if (!s || !words || nb_words <= 0 || !n)
-        return (NULL);
+	if (!s || !words || nb_words <= 0 || !n)
+		return (NULL);
 
-    word_len = strlen(words[0]);
-    s_len = strlen(s);
-    max_indices = s_len - (nb_words * word_len) + 1;
+	word_len = strlen(words[0]);
+	s_len = strlen(s);
+	max_indices = s_len - (nb_words * word_len) + 1;
 
-    if (max_indices <= 0)
-    {
-        *n = 0;
-        return (NULL);
-    }
+	if (max_indices <= 0)
+	{
+		*n = 0;
+		return (NULL);
+	}
 
-    indices = malloc(max_indices * sizeof(int));
-    used = malloc(nb_words * sizeof(int));
+	indices = malloc(max_indices * sizeof(int));
+	used = malloc(nb_words * sizeof(int));
 
-    if (!indices || !used)
-    {
-        free(indices);
-        free(used);
-        return (NULL);
-    }
+	if (!indices || !used)
+	{
+		free(indices);
+		free(used);
+		return (NULL);
+	}
 
-    for (i = 0; i <= s_len - (nb_words * word_len); i++)
-    {
-        if (check_substring(s, words, nb_words, word_len, used, i))
-        {
-            indices[count] = i;
-            count++;
-        }
-    }
+	for (i = 0; i <= s_len - (nb_words * word_len); i++)
+	{
+		if (check_substring(s, words, nb_words, word_len, used, i))
+		{
+			indices[count] = i;
+			count++;
+		}
+	}
 
-    free(used);
+	free(used);
 
-    if (count == 0)
-    {
-        free(indices);
-        indices = NULL;
-    }
-    else
-    {
-        int *temp = realloc(indices, count * sizeof(int));
-        if (temp)
-            indices = temp;
-    }
+	if (count == 0)
+	{
+		free(indices);
+		indices = NULL;
+	}
+	else
+	{
+		int *temp = realloc(indices, count * sizeof(int));
+		if (temp)
+			indices = temp;
+	}
 
-    *n = count;
-    return (indices);
+	*n = count;
+	return (indices);
 }
