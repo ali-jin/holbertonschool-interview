11e2e32304c0cc3ef64f94f2660c7101cd17c49e
ali-jin <alina.jin.contact@gmail.com>
2025-07-21 15:06:55 +0200

    creates a binary tree node

--- heap_insert/0-binary_tree_node.c
+++ heap_insert/0-binary_tree_node.c
@@ -9,15 +9,15 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-    if (node == NULL)
-        return (NULL);
+	if (node == NULL)
+		return (NULL);
 
-    node->parent = parent;
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
+	node->parent = parent;
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
 
-    return (node);
+	return (node);
 }
